73cf010c5f7f0dbae1306873bcbf46ff5507e3b1
Davies-K <john.daves13@gmail.com>
2021-07-11 10:51:43 +0000

    changed app title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Fendi Collection',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
